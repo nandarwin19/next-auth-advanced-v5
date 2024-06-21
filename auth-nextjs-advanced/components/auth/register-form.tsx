457e16c07c50981cedcf2589ff1b6a04239e6f52
nwin19 <nandarwinbuma@gmail.com>
2024-06-21 16:54:06 +0630

    Register form ✔️

--- auth-nextjs-advanced/components/auth/register-form.tsx
+++ auth-nextjs-advanced/components/auth/register-form.tsx
@@ -3,9 +3,6 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useSearchParams } from "next/navigation";
-import { useState, useTransition } from "react";
-import Link from "next/link";
 
 import {
   Form,
@@ -16,48 +13,71 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { CardWrapper } from "./card-wrapper";
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { Input } from "@/components/ui/input";
-import { Button } from "../ui/button";
-import { login } from "@/actions/login";
-import { FormError } from "../form-error";
-import { FormSuccess } from "../form-success";
+import { Button } from "@/components/ui/button";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { startTransition, useState, useTransition } from "react";
+import { register } from "@/actions/register";
 
-export default function LoginForm() {
+export const RegisterForm = () => {
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
     setError("");
     setSuccess("");
 
     startTransition(() => {
-      login(values).then((data) => {
+      register(values).then((data) => {
         setError(data.error);
         setSuccess(data.success);
       });
     });
+
+    form.reset;
   };
 
   return (
     <CardWrapper
-      headerLabel="Welcome Back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
+      headerLabel="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
       showSocial
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="John Doe"
+                      autoCorrect="off"
+                      autoComplete="off"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -93,9 +113,6 @@ export default function LoginForm() {
                       disabled={isPending}
                     />
                   </FormControl>
-                  <Button variant="link" size="sm" asChild className="px-0">
-                    <Link href="/auth/reset">Forgot password?</Link>
-                  </Button>
                   <FormMessage />
                 </FormItem>
               )}
@@ -103,11 +120,11 @@ export default function LoginForm() {
           </div>
           <FormError message={error} />
           <FormSuccess message={success} />
-          <Button type="submit" className="w-full">
-            Login
+          <Button type="submit" disabled={isPending} className="w-full">
+            Create Account
           </Button>
         </form>
       </Form>
     </CardWrapper>
   );
-}
+};
